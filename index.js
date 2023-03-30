9e8b0e173ede3863c32d4b03ef03a7656447d853
Joseph Ofei <josephofei24@gmail.com>
2023-03-29 20:49:23 -0400

    array reduce method complete

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (acc, element) { return acc + element }, 0)
+
+
+
+/*let arr = [10, 20, 30, 40];
+let sum = arr.reduce(function (a, b) {
+    return a + b;
+}, 0);
+console.log(sum);*/
